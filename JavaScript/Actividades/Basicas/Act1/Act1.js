8fa17c316b3b3069b2409af680bd30ab71cf1a65
Mizume25 <gabrielnivicela649@gmail.com>
2026-02-03 19:23:10 +0100

    nuevos ejercicios

--- JavaScript/Actividades/Basicas/Act1/Act1.js
+++ JavaScript/Actividades/Basicas/Act1/Act1.js
@@ -1,9 +1,15 @@
 //Interruptor
 const btn = document.getElementById("theme-toggle");
 const body = document.querySelector("body");
-
+btn.textContent = "off";
 //Evento: Quita y modifica la classe
 btn.addEventListener('click',function () {
     body.classList.toggle("light")
     body.classList.toggle("dark");
+    
+    if (btn.textContent === "on") {
+        btn.textContent = "off"
+    } else {
+        btn.textContent = "on"
+    }
 });
